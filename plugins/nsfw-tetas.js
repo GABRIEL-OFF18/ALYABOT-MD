efde9d5fa9be234236eae4ca8fbde6499a720d04
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-03-22 14:45:46 -0500

    Actualizar nsfw-tetas.js

--- plugins/nsfw-tetas.js
+++ plugins/nsfw-tetas.js
@@ -2,6 +2,8 @@ let handler = async(m, { conn }) => {
 
 let img = 'https://api.delirius.store/nsfw/boobs';
 
+if (!gp.nsfw && m.isGroup) return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
+
 let text = '*🍭 TETAS*';
 
 conn.sendMessage(m.chat, { image: { url: img }, caption: text }, { quoted: m });
